7b477ae81bb06c99a5a907b73f7a3bef89e3b89f
Ketan Patil <k2p1020@gmail.com>
2021-03-20 23:55:06 +0530

    CSV File feature

--- routes.py
+++ routes.py
@@ -4,17 +4,32 @@ from selenium import webdriver
 import os
 from time import sleep
 from scrapper import Scrapper
+from werkzeug.wrappers import Response
+import csv
 
 scrp = Scrapper()
 
 @app.route('/')
 def home():
     return render_template('index.html')
- 
+
+@app.route("/getPlotCSV")
+def getPlotCSV():
+    with open("CSV/data.csv") as f:
+        csv = f.read()
+    response = Response(csv, mimetype='text/csv')
+    response.headers.set("Content-Disposition", "attachment", filename="log.csv")
+    return response
+    
+
 @app.route('/search', methods=['POST'])
 def search():
     if request.method == "POST":
         search_term = request.form.get("searchquery")
         result, top_result = scrp.search_term(search_term)
+        with open('CSV/data.csv', 'w') as f:
+            for key in result.keys():
+                for v in result[key]:
+                    f.write("%s,%s\n"%(key,v))
         
     return render_template("index.html", result = result, top_result = top_result, searched = search_term)
