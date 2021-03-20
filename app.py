21f7f3dea0af1d54d93f28ebe45a8273315edcf8
Ketan Patil <k2p1020@gmail.com>
2021-03-20 08:20:44 +0530

    Lets Begin!

---
+++ app.py
@@ -0,0 +1,22 @@
+# importing Flask and other modules 
+from flask import Flask, request, render_template 
+import numpy as np
+import pandas as pd
+
+# Flask constructor 
+app = Flask(__name__) 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+ 
+@app.route('/search', methods=['POST'])
+def search():
+    if request.method == "POST":
+        name = request.form.get("searchquery")
+        
+    return render_template("index.html", name = name)
+	
+ 
+if __name__=='__main__':  
+    app.run(debug=True) 
