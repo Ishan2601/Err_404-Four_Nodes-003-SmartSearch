c0c589e4858017a0cc93acb6ebe8f9021a5ce04f
Ishan2601 <apiishan18@gmail.com>
2021-03-20 15:13:25 +0530

    Added another scraper

--- config.py
+++ config.py
@@ -3,3 +3,8 @@ class GoogleSearchConfig:
     search_box_name = "q"
     search_terms_box_class = "erkvQe"
     search_suggestions = "sbl1"
+
+class YahooSearchConfig:
+    url = "https://in.search.yahoo.com/"
+    search_box_name = "p"
+    search_terms_box_class = "sa-tray-list-container"
--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-from config import GoogleSearchConfig
+from config import GoogleSearchConfig, YahooSearchConfig
 from os import environ
+
+# from webdriver_manager.chrome import ChromeDriverManager
 
 class Scrapper:
     question_words = ["How", "What", "Why", "When", "Where", "Which", "Is"]
@@ -14,12 +16,25 @@ class Scrapper:
         self.options.add_argument('--no-sandbox')
         self.options.add_argument("--disable-dev-shm-usage")
         self.options.binary_location = environ.get("GOOGLE_CHROME_BIN")
-        self.brow = webdriver.Chrome(executable_path=environ.get("CHROMEDRIVER_PATH"),options=self.options) 
+        self.brow = webdriver.Chrome(executable_path=environ.get("CHROMEDRIVER_PATH"),options=self.options)
+        
+        # self.brow = webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=self.options)  
+
+    def __result_combiner(self, *args):
+        final_result = {i:[] for i in self.question_words}
+        for result in args:
+            for i in self.question_words:
+                if i in result:
+                    final_result[i].extend(result[i])
+        return final_result
 
     def search_term(self, search_term : str):
         google_search_ques = self.__get_google_search_ques(search_term)
+        yahoo_search_ques = self.__get_yahoo_search_ques(search_term)
+        
+        questions = self.__result_combiner(google_search_ques, yahoo_search_ques)
 
-        return google_search_ques
+        return questions
 
     def __get_google_search_ques(self, search_term : str):
         config = GoogleSearchConfig()
@@ -36,9 +51,30 @@ class Scrapper:
 
             web[prepend] = suggestions
             ques_box.clear()
-
         return web
 
+    def __get_yahoo_search_ques(self, search_term : str):
+        config = YahooSearchConfig()
+        self.brow.get(config.url)
+        web = {}
+
+        for prepend in self.question_words:
+            print(f"{prepend} {search_term}")
+            ques_box = self.brow.find_element_by_name(config.search_box_name)
+            ques_box.send_keys(f"{prepend} {search_term}")
+            sleep(2)
+            try:
+                search_term_box = self.brow.find_element_by_class_name(config.search_terms_box_class)
+                suggestions = search_term_box.text.split("\n")
+                web[prepend] = suggestions
+            except Exception:
+                print(prepend)
+                continue
+            
+            ques_box.clear()
+        return web
+
+    
 
 
 
