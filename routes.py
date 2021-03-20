6c799803cc2ea35684fbbd56edd71064177fe9ac
Ishan2601 <apiishan18@gmail.com>
2021-03-20 10:27:31 +0530

    App deployment on Heroku

--- routes.py
+++ routes.py
@@ -1,9 +1,15 @@
 from app import app
 from flask import request, render_template 
 from selenium import webdriver
+import os
 from time import sleep
-
+'''
 def scrape_google(text):
+	#text = "Elon Musk"
+
+	fireFoxOptions = webdriver.FirefoxOptions()
+	fireFoxOptions.set_headless()
+	browser = webdriver.Firefox('C:/WebDriver/bin', firefox_options=fireFoxOptions)
 	google_config = {
 	    'url':"https://www.google.co.in/",
 	    'search_box_name':"q",
@@ -11,12 +17,42 @@ def scrape_google(text):
 	    'search_suggestions':"sbl1"
 	}
 
-	#text = "Elon Musk"
 	question_words = ["How", "What", "Why", "When", "Where", "Which", "Is"]
 
-	fireFoxOptions = webdriver.FirefoxOptions()
-	fireFoxOptions.set_headless()
-	browser = webdriver.Firefox('C:/WebDriver/bin', firefox_options=fireFoxOptions)
+	browser.get(google_config['url'])
+
+	all_questions = {}
+
+	for question in question_words:
+	    ques_box = browser.find_element_by_name(google_config['search_box_name'])
+	    ques_box.send_keys(f"{question_words} {text}")
+	    sleep(1)
+	    search_term_box = browser.find_element_by_class_name(google_config['search_terms_box_class'])
+	    suggestions_elems = search_term_box.find_elements_by_class_name(google_config['search_suggestions'])
+	    suggestions = [i.text for i in suggestions_elems]
+
+	    all_questions[question] = suggestions
+	    ques_box.clear()
+	return all_questions
+'''
+def scrape(text):
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument("--no-sandbox")
+	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+	browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+	question_words = ["How", "What", "Why", "When", "Where", "Which", "Is"]
+
+	# Code for google scraping
+	google_config = {
+	    'url':"https://www.google.co.in/",
+	    'search_box_name':"q",
+	    'search_terms_box_class':"erkvQe",
+	    'search_suggestions':"sbl1"
+	}
+
 	browser.get(google_config['url'])
 
 	all_questions = {}
@@ -34,6 +70,7 @@ def scrape_google(text):
 	return all_questions
 
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -42,6 +79,6 @@ def home():
 def search():
     if request.method == "POST":
         search_term = request.form.get("searchquery")
-        result = scrape_google(search_term)
+        result = scrape(search_term)
         
     return render_template("index.html", result = result)
